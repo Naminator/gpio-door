6f2f4d3e359fcbf9fca15d826acc4e2570896be1
Lachezar Tsochev <ltsochev@live.com>
2015-10-03 18:46:38 +0300

    Filesystem

---
+++ include/GarageDoor/Filesystem.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "stdafx.h"
+
+namespace GarageDoor {
+
+    std::vector<std::string> ListDirectory(tstring);
+
+}
---
+++ src/GarageDoor/Filesystem.cpp
@@ -0,0 +1,21 @@
+#include "GarageDoor/Filesystem.h"
+
+std::vector<std::string> GarageDoor::ListDirectory(tstring path)
+{
+    std::vector<std::string> dirlist;
+
+    DIR * dir = opendir(path.c_str());
+    struct dirent * entry = readdir(dir);
+
+    while (entry != NULL)
+    {
+        std::stringstream ss;
+        ss << entry->d_name;
+
+        dirlist.push_back(ss.str());
+        entry = readdir(dir);
+    }
+
+    closedir(dir);
+    return dirlist;
+}
